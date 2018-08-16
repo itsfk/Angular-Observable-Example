3274f7f8a687bb011f726e9eb927d8a044ae603e
Faiz <faiz.ggmu@gmail.com>
2018-08-16 22:21:59 +0500

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { EmployeeDetailComponent } from './employee-detail/employee-detail.component';
+import { EmployeeService } from './employee.service';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/employee.service.ts
@@ -0,0 +1,22 @@
+import {throwError as observableThrowError,  Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { IEmployee } from './employee';
+import { tap, catchError } from 'rxjs/operators';
+
+@Injectable()
+export class EmployeeService {
+
+  private _url = '/assets/data/employees.json';
+
+  constructor(private http: HttpClient) { }
+// Cast Observable into Employee Aray
+  getEmployees(): Observable<IEmployee[]>  {
+    return this.http.get<IEmployee[]>(this._url)
+                    .pipe(tap(data => alert(JSON.stringify(data))) , catchError(this.errorHandler));
+  }
+  errorHandler(error: HttpErrorResponse) {
+    return observableThrowError(error.message || 'Server Error');
+  }
+
+}
